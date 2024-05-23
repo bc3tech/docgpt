ed1a7c51dfd74153f82968772f40c5396afa57be
Brandon🌩️H <bc3tech@users.noreply.github.com>
2024-05-23 12:18:51 -0700

    bumping nupkgs

---
+++ DocGpt.Test/GptSetup.cs
@@ -0,0 +1,17 @@
+﻿namespace DocGpt.Test;
+
+using DocGpt.Options;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+internal static class GptSetup
+{
+    [AssemblyInitialize]
+    public static void AsmInit(TestContext _)
+    {
+        DocGptOptions.Instance.Endpoint = new("https://api.openai.com");
+        DocGptOptions.Instance.ApiKey = "...";
+        DocGptOptions.Instance.ModelDeploymentName = "gpt-3.5-turbo-16k";
+    }
+}
